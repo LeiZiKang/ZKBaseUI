94691f826fa66ef6d9f082e4e2b9519a26cdce92
Levi Lei <levilei@esdlife.com>
2025-01-24 15:17:54 +0800

    fix: UIKit 的访问控制和 swiftUI 的不同，class 的 inhert 要改为 open

--- Sources/ZKBaseUI/UIKit/BaseViewController.swift
+++ Sources/ZKBaseUI/UIKit/BaseViewController.swift
@@ -7,16 +7,23 @@
 
 import UIKit
 
-class BaseViewController: UIViewController {
+open class BaseViewController: UIViewController {
 
-
-    override func viewDidLoad() {
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    open override func viewDidLoad() {
         super.viewDidLoad()
         setUpUI()
     }
     
     private func setUpUI() {
-        
         // background
         view.backgroundColor = .systemBackground
         
@@ -39,7 +46,7 @@ class BaseViewController: UIViewController {
 
 #Preview {
     let testVC = BaseViewController()
-        .backGroundColor(.systemBackground)
+//        .backGroundColor(.systemBackground)
         .navTitle("你好")
     
     return UINavigationController(rootViewController: testVC)
